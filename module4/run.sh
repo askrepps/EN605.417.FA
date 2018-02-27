7cff14637da4991ad69d0f0407f7d6f5d4608faf
askrepps <askrepps@gmail.com>
2018-02-26 22:03:11 -0500

    Updated run.sh to use multiple block sizes

--- module4/run.sh
+++ module4/run.sh
@@ -25,16 +25,27 @@ if [ $? -ne 0 ]; then
         exit 1
 fi
 
-runTest 256 256
 runTest 512 256
+runTest 512 512
 runTest 1024 256
+runTest 1024 512
 runTest 2048 256
+runTest 2048 512
 runTest 4096 256
+runTest 4096 512
 runTest 8192 256
+runTest 8192 512
 runTest 16384 256
+runTest 16384 512
 runTest 32768 256
+runTest 32768 512
 runTest 65536 256
+runTest 65536 512
 runTest 131072 256
+runTest 131072 512
 runTest 262144 256
+runTest 262144 512
 runTest 524288 256
+runTest 524288 512
 runTest 1048576 256
+runTest 1048576 512
