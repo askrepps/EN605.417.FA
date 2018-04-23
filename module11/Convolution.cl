5849464b71d7f41057869de4459c5d7bba5cf591
Andrew Krepps <Andrew.Krepps@jhuapl.edu>
2018-04-23 17:27:21 -0400

    Updated convolution program to use floats and reworked work item and data indexing

--- module11/Convolution.cl
+++ module11/Convolution.cl
@@ -1,37 +1,48 @@
-//
-// Book:      OpenCL(R) Programming Guide
-// Authors:   Aaftab Munshi, Benedict Gaster, Timothy Mattson, James Fung, Dan Ginsburg
-// ISBN-10:   0-321-74964-2
-// ISBN-13:   978-0-321-74964-2
-// Publisher: Addison-Wesley Professional
-// URLs:      http://safari.informit.com/9780132488006/
-//            http://www.openclprogrammingguide.com
-//
+//////////////////////////
+// Convolution.cl       //
+// Andrew Krepps        //
+// Module 11 Assignment //
+// 23 April 2018        //
+//////////////////////////
 
-// Convolution.cl
-//
-//    This is a simple kernel performing convolution.
-
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Perform 2D signal convolution for a given output signal value
+/// 
+/// Note: This assumes that all data is in row-major order, the filter mask is
+/// square, and the input signal is the same size or larger than the output
+/// signal (depending on the mask size) such that all input locations read are
+/// valid and no zero-padding is required.
+/// 
+/// \param [in] input the input signal
+/// \param [in] mask the convolution mask
+/// \param [out] output the output signal
+/// \param [in] inputWidth the width of the input signal
+/// \param [in] maskWidth the width of the filter mask
+///////////////////////////////////////////////////////////////////////////////
 __kernel void convolve(
-	const __global  uint * const input,
-    __constant uint * const mask,
-    __global  uint * const output,
+    const __global  float * const input,
+    __constant float * const mask,
+    __global  float * const output,
     const int inputWidth,
     const int maskWidth)
 {
-    const int x = get_global_id(0);
-    const int y = get_global_id(1);
-
-    uint sum = 0;
-    for (int r = 0; r < maskWidth; r++)
-    {
-        const int idxIntmp = (y + r) * inputWidth + x;
-
-        for (int c = 0; c < maskWidth; c++)
-        {
-			sum += mask[(r * maskWidth)  + c] * input[idxIntmp + c];
-        }
-    } 
+    // get output coordinates (work items and all data are in row-major order)
+    const int outRow = get_global_id(0);
+    const int outCol = get_global_id(1);
     
-	output[y * get_global_size(0) + x] = sum;
+    // generate output sum using each input element's contribution
+    float sum = 0.0f;
+    for (int maskRow = 0; maskRow < maskWidth; maskRow++)
+    {
+        // pre-calculate start of the subset of the input row for this row of the mask
+        const int inRowStart = (maskRow + outRow)*inputWidth + outCol;
+        
+        for (int maskCol = 0; maskCol < maskWidth; maskCol++)
+        {
+            sum += mask[maskRow*maskWidth + maskCol] * input[inRowStart + maskCol];
+        }
+    }
+    
+    // set output signal value
+    output[outRow*get_global_size(0) + outCol] = sum;
 }
--- module11/Convolution.cpp
+++ module11/Convolution.cpp
@@ -1,18 +1,9 @@
-//
-// Book:      OpenCL(R) Programming Guide
-// Authors:   Aaftab Munshi, Benedict Gaster, Timothy Mattson, James Fung, Dan Ginsburg
-// ISBN-10:   0-321-74964-2
-// ISBN-13:   978-0-321-74964-2
-// Publisher: Addison-Wesley Professional
-// URLs:      http://safari.informit.com/9780132488006/
-//            http://www.openclprogrammingguide.com
-//
-
-
-// Convolution.cpp
-//
-//    This is a simple example that demonstrates OpenCL platform, device, and context
-//    use.
+//////////////////////////
+// Convolution.cpp      //
+// Andrew Krepps        //
+// Module 11 Assignment //
+// 23 April 2018        //
+//////////////////////////
 
 #include <iostream>
 #include <fstream>
@@ -25,37 +16,36 @@
 #include <CL/cl.h>
 #endif
 
-#if !defined(CL_CALLBACK)
-#define CL_CALLBACK
-#endif
-
 // Constants
-const unsigned int inputSignalWidth  = 8;
-const unsigned int inputSignalHeight = 8;
 
-cl_uint inputSignal[inputSignalWidth][inputSignalHeight] =
+// input signal (defined in row-major order)
+const unsigned int INPUT_SIGNAL_HEIGHT = 8;
+const unsigned int INPUT_SIGNAL_WIDTH  = 8;
+cl_float inputSignal[INPUT_SIGNAL_HEIGHT][INPUT_SIGNAL_WIDTH] =
 {
-	{3, 1, 1, 4, 8, 2, 1, 3},
-	{4, 2, 1, 1, 2, 1, 2, 3},
-	{4, 4, 4, 4, 3, 2, 2, 2},
-	{9, 8, 3, 8, 9, 0, 0, 0},
-	{9, 3, 3, 9, 0, 0, 0, 0},
-	{0, 9, 0, 8, 0, 0, 0, 0},
-	{3, 0, 8, 8, 9, 4, 4, 4},
-	{5, 9, 8, 1, 8, 1, 1, 1}
+    {3.0f, 1.0f, 1.0f, 4.0f, 8.0f, 2.0f, 1.0f, 3.0f},
+    {4.0f, 2.0f, 1.0f, 1.0f, 2.0f, 1.0f, 2.0f, 3.0f},
+    {4.0f, 4.0f, 4.0f, 4.0f, 3.0f, 2.0f, 2.0f, 2.0f},
+    {9.0f, 8.0f, 3.0f, 8.0f, 9.0f, 0.0f, 0.0f, 0.0f},
+    {9.0f, 3.0f, 3.0f, 9.0f, 0.0f, 0.0f, 0.0f, 0.0f},
+    {0.0f, 9.0f, 0.0f, 8.0f, 0.0f, 0.0f, 0.0f, 0.0f},
+    {3.0f, 0.0f, 8.0f, 8.0f, 9.0f, 4.0f, 4.0f, 4.0f},
+    {5.0f, 9.0f, 8.0f, 1.0f, 8.0f, 1.0f, 1.0f, 1.0f}
 };
 
-const unsigned int outputSignalWidth  = 6;
-const unsigned int outputSignalHeight = 6;
+// output signal (defined in row-major order)
+const unsigned int OUTPUT_SIGNAL_HEIGHT = 6;
+const unsigned int OUTPUT_SIGNAL_WIDTH  = 6;
+cl_float outputSignal[OUTPUT_SIGNAL_HEIGHT][OUTPUT_SIGNAL_WIDTH];
 
-cl_uint outputSignal[outputSignalWidth][outputSignalHeight];
-
-const unsigned int maskWidth  = 3;
-const unsigned int maskHeight = 3;
-
-cl_uint mask[maskWidth][maskHeight] =
+// convolution mask (defined in row-major order)
+const unsigned int MASK_HEIGHT = 3;
+const unsigned int MASK_WIDTH  = 3;
+cl_float mask[MASK_HEIGHT][MASK_WIDTH] =
 {
-	{1, 1, 1}, {1, 0, 1}, {1, 1, 1},
+    {1.0f, 1.0f, 1.0f},
+    {1.0f, 0.0f, 1.0f},
+    {1.0f, 1.0f, 1.0f}
 };
 
 ///
@@ -69,86 +59,68 @@ checkErr(cl_int err, const char * name)
     }
 }
 
-void CL_CALLBACK contextCallback(
-	const char * errInfo,
-	const void * private_info,
-	size_t cb,
-	void * user_data)
-{
-	std::cout << "Error occured during context use: " << errInfo << std::endl;
-	// should really perform any clearup and so on at this point
-	// but for simplicitly just exit.
-	exit(1);
-}
-
 ///
-//	main() for Convoloution example
+//  main() for Convoloution example
 //
 int main(int argc, char** argv)
 {
     cl_int errNum;
     cl_uint numPlatforms;
-	cl_uint numDevices;
+    cl_uint numDevices;
     cl_platform_id * platformIDs;
-	cl_device_id * deviceIDs;
+    cl_device_id * deviceIDs;
     cl_context context = NULL;
-	cl_command_queue queue;
-	cl_program program;
-	cl_kernel kernel;
-	cl_mem inputSignalBuffer;
-	cl_mem outputSignalBuffer;
-	cl_mem maskBuffer;
-
+    cl_command_queue queue;
+    cl_program program;
+    cl_kernel kernel;
+    cl_mem inputSignalBuffer;
+    cl_mem outputSignalBuffer;
+    cl_mem maskBuffer;
+    
     // First, select an OpenCL platform to run on.  
-	errNum = clGetPlatformIDs(0, NULL, &numPlatforms);
-	checkErr( 
-		(errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS), 
-		"clGetPlatformIDs"); 
- 
-	platformIDs = (cl_platform_id *)alloca(
-       		sizeof(cl_platform_id) * numPlatforms);
-
+    errNum = clGetPlatformIDs(0, NULL, &numPlatforms);
+    checkErr( 
+        (errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS), 
+        "clGetPlatformIDs"); 
+    
+    platformIDs = (cl_platform_id *)alloca(
+            sizeof(cl_platform_id) * numPlatforms);
+    
     errNum = clGetPlatformIDs(numPlatforms, platformIDs, NULL);
     checkErr( 
-	   (errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS), 
-	   "clGetPlatformIDs");
-
-	// Iterate through the list of platforms until we find one that supports
-	// a CPU device, otherwise fail with an error.
-	deviceIDs = NULL;
-	cl_uint i;
-	for (i = 0; i < numPlatforms; i++)
-	{
-		errNum = clGetDeviceIDs(
+       (errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS), 
+       "clGetPlatformIDs");
+    
+    // Iterate through the list of platforms until we find one that supports
+    // a CPU device, otherwise fail with an error.
+    deviceIDs = NULL;
+    cl_uint i;
+    for (i = 0; i < numPlatforms; i++)
+    {
+        errNum = clGetDeviceIDs(
             platformIDs[i], 
             CL_DEVICE_TYPE_GPU, 
             0,
             NULL,
             &numDevices);
-		if (errNum != CL_SUCCESS && errNum != CL_DEVICE_NOT_FOUND)
-	    {
-			checkErr(errNum, "clGetDeviceIDs");
+        if (errNum != CL_SUCCESS && errNum != CL_DEVICE_NOT_FOUND)
+        {
+            checkErr(errNum, "clGetDeviceIDs");
         }
-	    else if (numDevices > 0) 
-	    {
-		   	deviceIDs = (cl_device_id *)alloca(sizeof(cl_device_id) * numDevices);
-			errNum = clGetDeviceIDs(
-				platformIDs[i],
-				CL_DEVICE_TYPE_GPU,
-				numDevices, 
-				&deviceIDs[0], 
-				NULL);
-			checkErr(errNum, "clGetDeviceIDs");
-			break;
-	   }
-	}
-
-	// Check to see if we found at least one CPU device, otherwise return
-// 	if (deviceIDs == NULL) {
-// 		std::cout << "No CPU device found" << std::endl;
-// 		exit(-1);
-// 	}
-
+        else if (numDevices > 0) 
+        {
+            deviceIDs = (cl_device_id *)alloca(sizeof(cl_device_id) * numDevices);
+            errNum = clGetDeviceIDs(
+                platformIDs[i],
+                CL_DEVICE_TYPE_GPU,
+                numDevices, 
+                &deviceIDs[0], 
+                NULL);
+            checkErr(errNum, "clGetDeviceIDs");
+            break;
+       }
+    }
+    
     // Next, create an OpenCL context on the selected platform.  
     cl_context_properties contextProperties[] =
     {
@@ -157,144 +129,144 @@ int main(int argc, char** argv)
         0
     };
     context = clCreateContext(
-		contextProperties, 
-		numDevices,
+        contextProperties, 
+        numDevices,
         deviceIDs, 
-		&contextCallback,
-		NULL, 
-		&errNum);
-	checkErr(errNum, "clCreateContext");
-
-	std::ifstream srcFile("Convolution.cl");
+        NULL,
+        NULL, 
+        &errNum);
+    checkErr(errNum, "clCreateContext");
+    
+    std::ifstream srcFile("Convolution.cl");
     checkErr(srcFile.is_open() ? CL_SUCCESS : -1, "reading Convolution.cl");
-
-	std::string srcProg(
+    
+    std::string srcProg(
         std::istreambuf_iterator<char>(srcFile),
         (std::istreambuf_iterator<char>()));
-
-	const char * src = srcProg.c_str();
-	size_t length = srcProg.length();
-
-	// Create program from source
-	program = clCreateProgramWithSource(
-		context, 
-		1, 
-		&src, 
-		&length, 
-		&errNum);
-	checkErr(errNum, "clCreateProgramWithSource");
-
-	// Build program
-	errNum = clBuildProgram(
-		program,
-		numDevices,
-		deviceIDs,
-		NULL,
-		NULL,
-		NULL);
+    
+    const char * src = srcProg.c_str();
+    size_t length = srcProg.length();
+    
+    // Create program from source
+    program = clCreateProgramWithSource(
+        context, 
+        1, 
+        &src, 
+        &length, 
+        &errNum);
+    checkErr(errNum, "clCreateProgramWithSource");
+    
+    // Build program
+    errNum = clBuildProgram(
+        program,
+        numDevices,
+        deviceIDs,
+        NULL,
+        NULL,
+        NULL);
     if (errNum != CL_SUCCESS)
     {
         // Determine the reason for the error
         char buildLog[16384];
         clGetProgramBuildInfo(
-			program, 
-			deviceIDs[0], 
-			CL_PROGRAM_BUILD_LOG,
+            program, 
+            deviceIDs[0], 
+            CL_PROGRAM_BUILD_LOG,
             sizeof(buildLog), 
-			buildLog, 
-			NULL);
-
+            buildLog, 
+            NULL);
+        
         std::cerr << "Error in kernel: " << std::endl;
         std::cerr << buildLog;
-		checkErr(errNum, "clBuildProgram");
+        checkErr(errNum, "clBuildProgram");
     }
-
-	// Create kernel object
-	kernel = clCreateKernel(
-		program,
-		"convolve",
-		&errNum);
-	checkErr(errNum, "clCreateKernel");
-
-	// Now allocate buffers
-	inputSignalBuffer = clCreateBuffer(
-		context,
-		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-		sizeof(cl_uint) * inputSignalHeight * inputSignalWidth,
-		static_cast<void *>(inputSignal),
-		&errNum);
-	checkErr(errNum, "clCreateBuffer(inputSignal)");
-
-	maskBuffer = clCreateBuffer(
-		context,
-		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-		sizeof(cl_uint) * maskHeight * maskWidth,
-		static_cast<void *>(mask),
-		&errNum);
-	checkErr(errNum, "clCreateBuffer(mask)");
-
-	outputSignalBuffer = clCreateBuffer(
-		context,
-		CL_MEM_WRITE_ONLY,
-		sizeof(cl_uint) * outputSignalHeight * outputSignalWidth,
-		NULL,
-		&errNum);
-	checkErr(errNum, "clCreateBuffer(outputSignal)");
-
-	// Pick the first device and create command queue.
-	queue = clCreateCommandQueue(
-		context,
-		deviceIDs[0],
-		0,
-		&errNum);
-	checkErr(errNum, "clCreateCommandQueue");
-
+    
+    // Create kernel object
+    kernel = clCreateKernel(
+        program,
+        "convolve",
+        &errNum);
+    checkErr(errNum, "clCreateKernel");
+    
+    // Now allocate buffers
+    inputSignalBuffer = clCreateBuffer(
+        context,
+        CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+        sizeof(cl_float) * INPUT_SIGNAL_HEIGHT * INPUT_SIGNAL_WIDTH,
+        static_cast<void *>(inputSignal),
+        &errNum);
+    checkErr(errNum, "clCreateBuffer(inputSignal)");
+    
+    maskBuffer = clCreateBuffer(
+        context,
+        CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+        sizeof(cl_float) * MASK_HEIGHT * MASK_WIDTH,
+        static_cast<void *>(mask),
+        &errNum);
+    checkErr(errNum, "clCreateBuffer(mask)");
+    
+    outputSignalBuffer = clCreateBuffer(
+        context,
+        CL_MEM_WRITE_ONLY,
+        sizeof(cl_float) * OUTPUT_SIGNAL_HEIGHT * OUTPUT_SIGNAL_WIDTH,
+        NULL,
+        &errNum);
+    checkErr(errNum, "clCreateBuffer(outputSignal)");
+    
+    // Pick the first device and create command queue.
+    queue = clCreateCommandQueue(
+        context,
+        deviceIDs[0],
+        0,
+        &errNum);
+    checkErr(errNum, "clCreateCommandQueue");
+    
     errNum  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputSignalBuffer);
-	errNum |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &maskBuffer);
+    errNum |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &maskBuffer);
     errNum |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &outputSignalBuffer);
-	errNum |= clSetKernelArg(kernel, 3, sizeof(cl_uint), &inputSignalWidth);
-	errNum |= clSetKernelArg(kernel, 4, sizeof(cl_uint), &maskWidth);
-	checkErr(errNum, "clSetKernelArg");
-
-	const size_t globalWorkSize[1] = { outputSignalWidth * outputSignalHeight };
-    const size_t localWorkSize[1]  = { 1 };
-
+    errNum |= clSetKernelArg(kernel, 3, sizeof(cl_uint), &INPUT_SIGNAL_WIDTH);
+    errNum |= clSetKernelArg(kernel, 4, sizeof(cl_uint), &MASK_WIDTH);
+    checkErr(errNum, "clSetKernelArg");
+    
+    const size_t globalWorkSize[2] = { OUTPUT_SIGNAL_HEIGHT, OUTPUT_SIGNAL_WIDTH };
+    const size_t localWorkSize[2]  = { 1, 1 };
+    
     // Queue the kernel up for execution across the array
     errNum = clEnqueueNDRangeKernel(
-		queue, 
-		kernel, 
-		1, 
-		NULL,
+        queue, 
+        kernel, 
+        2, 
+        NULL,
         globalWorkSize, 
-		localWorkSize,
+        localWorkSize,
         0, 
-		NULL, 
-		NULL);
-	checkErr(errNum, "clEnqueueNDRangeKernel");
+        NULL, 
+        NULL);
+    checkErr(errNum, "clEnqueueNDRangeKernel");
     
-	errNum = clEnqueueReadBuffer(
-		queue, 
-		outputSignalBuffer, 
-		CL_TRUE,
+    errNum = clEnqueueReadBuffer(
+        queue, 
+        outputSignalBuffer, 
+        CL_TRUE,
         0, 
-		sizeof(cl_uint) * outputSignalHeight * outputSignalHeight, 
-		outputSignal,
+        sizeof(cl_float) * OUTPUT_SIGNAL_HEIGHT * OUTPUT_SIGNAL_WIDTH, 
+        outputSignal,
         0, 
-		NULL, 
-		NULL);
-	checkErr(errNum, "clEnqueueReadBuffer");
-
+        NULL, 
+        NULL);
+    checkErr(errNum, "clEnqueueReadBuffer");
+    
     // Output the result buffer
-    for (int y = 0; y < outputSignalHeight; y++)
-	{
-		for (int x = 0; x < outputSignalWidth; x++)
-		{
-			std::cout << outputSignal[x][y] << " ";
-		}
-		std::cout << std::endl;
-	}
-
+    for (int y = 0; y < OUTPUT_SIGNAL_HEIGHT; y++)
+    {
+        for (int x = 0; x < OUTPUT_SIGNAL_WIDTH; x++)
+        {
+            std::cout << outputSignal[y][x] << " ";
+        }
+        std::cout << std::endl;
+    }
+    
     std::cout << std::endl << "Executed program succesfully." << std::endl;
-
-	return 0;
+    
+    return 0;
 }
