565b8dada3cc8df799a9e5b9b1348cfd13d1f65b
Andrew Krepps <Andrew.Krepps@jhuapl.edu>
2018-04-23 18:35:57 -0400

    Added run script

---
+++ module11/run.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+runTest()
+{
+	#arraySize=$1
+	
+	#echo "Running test with array size of $arraySize..."
+	echo "Running test..."
+	build/Convolution
+	echo ""
+}
+
+echo 'Creating build directory...'
+if [ ! -d build ]; then
+	mkdir build
+	if [ $? -ne 0 ]; then
+		echo 'Could not create build directory' >&2
+		exit 1
+	fi
+fi
+
+echo 'Compiling...'
+CXXFLAGS='-std=c++11'
+if [[ $OSTYPE == darwin* ]]; then
+	LINKFLAGS='-framework OpenCL'
+else
+	CXXFLAGS="$CXXFLAGS -I/usr/local/cuda/include -L/usr/local/cuda/lib64"
+	LINKFLAGS='-lOpenCL'
+fi
+g++ $CXXFLAGS -o build/Convolution Convolution.cpp $LINKFLAGS
+if [ $? -ne 0 ]; then
+        echo 'Compilation failed' >&2
+        exit 1
+fi
+echo ''
+
+echo 'Running tests...'
+runTest
