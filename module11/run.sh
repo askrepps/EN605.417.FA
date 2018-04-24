4fbf87b4271e6c2f99b5ae38488b1d9ae3a243e7
Andrew Krepps <Andrew.Krepps@jhuapl.edu>
2018-04-23 20:45:29 -0400

    Updated run script

--- module11/run.sh
+++ module11/run.sh
@@ -2,30 +2,29 @@
 
 runTest()
 {
-	#arraySize=$1
-	
-	#echo "Running test with array size of $arraySize..."
-	echo "Running test..."
-	build/Convolution
-	echo ""
+    inputFile=$1
+    
+    echo "Running test using $inputFile..."
+    build/Convolution $inputFile
+    echo ""
 }
 
 echo 'Creating build directory...'
 if [ ! -d build ]; then
-	mkdir build
-	if [ $? -ne 0 ]; then
-		echo 'Could not create build directory' >&2
-		exit 1
-	fi
+    mkdir build
+    if [ $? -ne 0 ]; then
+        echo 'Could not create build directory' >&2
+        exit 1
+    fi
 fi
 
 echo 'Compiling...'
 CXXFLAGS='-std=c++11'
 if [[ $OSTYPE == darwin* ]]; then
-	LINKFLAGS='-framework OpenCL'
+    LINKFLAGS='-framework OpenCL'
 else
-	CXXFLAGS="$CXXFLAGS -I/usr/local/cuda/include -L/usr/local/cuda/lib64"
-	LINKFLAGS='-lOpenCL'
+    CXXFLAGS="$CXXFLAGS -I/usr/local/cuda/include -L/usr/local/cuda/lib64"
+    LINKFLAGS='-lOpenCL'
 fi
 g++ $CXXFLAGS -o build/Convolution Convolution.cpp $LINKFLAGS
 if [ $? -ne 0 ]; then
@@ -35,4 +34,6 @@ fi
 echo ''
 
 echo 'Running tests...'
-runTest
+runTest data/inputSignal1.csv
+runTest data/inputSignal2.csv
+runTest data/inputSignal3.csv
